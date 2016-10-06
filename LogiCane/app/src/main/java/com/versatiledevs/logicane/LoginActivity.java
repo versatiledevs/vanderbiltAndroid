71cf32acee2d8e723227ebd62210aced1bcaef11
wintersina <wintersina@gmail.com>
2016-10-06 08:30:36 -0500

    opt

--- LogiCane/app/src/main/java/com/versatiledevs/logicane/LoginActivity.java
+++ LogiCane/app/src/main/java/com/versatiledevs/logicane/LoginActivity.java
@@ -1,5 +1,18 @@
+/*
+    Android app is developed by VersatileDevs
+        Lewis
+        Sina Serati
+        Jorden
+        PJ
+        Geo
+
+        Latest update on Oct/6/2016
+
+ */
+
 package com.versatiledevs.logicane;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +21,6 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,14 +30,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText editText_Email;
-    private EditText editText_Password;
-    private Button button_SignIN;
-    //private Button button_Register;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonSignIN;
 
     private FirebaseAuth firebaseAuth;
-    private ProgressBar progressBar;
+    private ProgressDialog progressDialog;
 
+    // This fuction is called when the App is opened, aka its the apps constructor
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,67 +49,61 @@ public class LoginActivity extends AppCompatActivity {
             finish();
         }
 
-        editText_Email = (EditText) findViewById(R.id.editTextEmail);
-        editText_Password = (EditText) findViewById(R.id.editTextPassword);
-        button_SignIN = (Button) findViewById(R.id.buttonSignIN);
-        //button_Register = (Button) findViewById(R.id.buttonSignup);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        editTextEmail = (EditText) findViewById(R.id.edit_email_text);
+        editTextPassword = (EditText) findViewById(R.id.edit_password_text);
+        buttonSignIN = (Button) findViewById(R.id.sign_in_button);
+        progressDialog = new ProgressDialog(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
-/*
-        button_Register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "No Activity for Signup", Toast.LENGTH_SHORT).show();
-                //startActivity(new Intent(LoginActivity.this, SignupActivity.class));
-            }
-        });
 
-*/
 
-        button_SignIN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = editText_Email.getText().toString();
-                final String password = editText_Password.getText().toString();
-
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                progressBar.setVisibility(View.VISIBLE);
-
-                //authenticate user
-                firebaseAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
-                                progressBar.setVisibility(View.GONE);
-                                if (!task.isSuccessful()) {
-                                    // there was an error
-                                    if (password.length() < 6) {
-                                        editText_Password.setError(getString(R.string.minimum_password));
-                                    } else {
-                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
-                                    }
-                                } else {
-                                    // MainActivity = sign out
-                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                }
-                            }
-                        });
-            }
-        });
     }
+    // this method will log user into firebase
+    public void login(View view) {
+        String email = editTextEmail.getText().toString();
+        final String password = editTextPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Logging in....");
+        progressDialog.show();
+
+        //authenticate user
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+
+                        if (!task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            // there was an error
+                            if (password.length() < 6) {
+                                editTextPassword.setError(getString(R.string.minimum_password));
+                            } else {
+                                Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            progressDialog.dismiss();
+                            // MainActivity = sign out
+                            Toast.makeText(LoginActivity.this, "WELCOME!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+    }
+
+
 }
--- LogiCane/app/src/main/java/com/versatiledevs/logicane/MainActivity.java
+++ LogiCane/app/src/main/java/com/versatiledevs/logicane/MainActivity.java
@@ -1,3 +1,15 @@
+/*
+    Android app is developed by VersatileDevs
+        Lewis
+        Sina Serati
+        Jorden
+        PJ
+        Geo
+
+        Latest update on Oct/6/2016
+
+ */
+
 package com.versatiledevs.logicane;
 
 import android.content.Intent;
@@ -6,28 +18,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button_SignOut;
+    private Button buttonSignOut;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // assign button
-        button_SignOut = (Button) findViewById(R.id.buttonSignOut);
+        buttonSignOut = (Button) findViewById(R.id.buttonSignOut);
 
         //get firebase auth instance
         firebaseAuth = FirebaseAuth.getInstance();
-
-        //final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -45,22 +54,12 @@ public class MainActivity extends AppCompatActivity {
             }
 
         };
-
-        button_SignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOut();      //function call
-
-            }
-        });
-
     }
 
-
-
     //sign out method
-    public void signOut() {
+    public void signOut(View view) {
         firebaseAuth.signOut();
+        Toast.makeText(MainActivity.this, "Goodbye.", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
         finish();
     }
