ab1dc886aa2c9499d58f4421b4979cc8e53d8422
pjkirby <pj.kirby07@gmail.com>
2016-10-19 02:39:19 -0500

    Messed around with some of the features of the database. I have worked out how to gather information from the database and am in the process of setting up the different activities.

--- LogiCane/app/src/main/java/com/versatiledevs/logicane/LoginActivity.java
+++ LogiCane/app/src/main/java/com/versatiledevs/logicane/LoginActivity.java
@@ -20,19 +20,30 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+import java.util.Objects;
+
+import static android.R.attr.data;
+import static android.R.attr.value;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -42,6 +53,11 @@ public class LoginActivity extends AppCompatActivity {
     private Button buttonSignIN;
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
+    public static int admin = 1;
+    public static int patient = 2;
+    public static int therapist = 3;
+    public static int role = 0;
+
 
     // This fuction is called when the App is opened, aka its the apps constructor
     @Override
@@ -55,8 +71,8 @@ public class LoginActivity extends AppCompatActivity {
             // it would take me back to this activity which is not what we want.
             //However, with it commented out it takes us back to the login screen which I think
             // is more appropriate.
-            /*
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+            /*startActivity(new Intent(LoginActivity.this, MainActivity.class));
             finish();*/
         }
         firebaseAuth = FirebaseAuth.getInstance();
@@ -70,6 +86,8 @@ public class LoginActivity extends AppCompatActivity {
 
     // this method will log user into firebase
     private void login() {
+
+
 
         editTextEmail = (EditText) findViewById(R.id.edit_email_text);
         editTextPassword = (EditText) findViewById(R.id.edit_password_text);
@@ -113,16 +131,45 @@ public class LoginActivity extends AppCompatActivity {
                             // MainActivity = sign out
                             Toast.makeText(LoginActivity.this, "WELCOME!", Toast.LENGTH_SHORT).show();
 
-                            //I need to put some if statements here to check and see what
-                            // the role of the user is.
-
-                            DatabaseReference ref = new DatabaseReference("https://myfirebaseurl.firebaseio.com/android/saving-data/fireblog");
-
-
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            /************************
+                             * This is the beginning of getting the role objects value from the Database.
+                             * I'm currently able to get the information from the database but
+                             * I'm unable to get the true value on the if statement below.
+                             */
+                            final FirebaseUser user = firebaseAuth.getCurrentUser();
+                            //Is this NULL?
                             String uid = user.getUid();
 
-                            if (true) //if user == admin go to admin activity
+                            final FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myRef = database.getReference("Users").child(uid);
+                            DatabaseReference fName = myRef.child("role");
+                            fName.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                    String temp = new String(dataSnapshot.getValue(String.class));
+                                    if(temp.equals("admin"))
+                                    {
+                                        LoginActivity.role = LoginActivity.admin;
+                                    }
+                                    //Log.d("Role:",role);
+                                    //Log.d("Admin?: ", admin);
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+
+                            //Log.d("Role1",role);
+                            //Log.d("Admin1", admin);
+                            /*
+                            *The if statement below is not producing the correct true value.
+                            * I'm still trying to figure this out.
+                            *
+                             */
+                            if (LoginActivity.role == LoginActivity.admin) //if user == admin go to admin activity
                             {
                                 Intent intent = new Intent(LoginActivity.this, Admin.class);
                                 startActivity(intent);
@@ -133,9 +180,9 @@ public class LoginActivity extends AppCompatActivity {
 
                             }else //User is a doctor.
                             {
-                                /*Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                 startActivity(intent);
-                                finish();*/
+                                finish();
                             }
 
 
