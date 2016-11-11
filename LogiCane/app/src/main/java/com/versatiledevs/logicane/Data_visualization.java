a587d2f54f593eb2ece0d077f95969b6233c27fa
Luis Chunga <lmc6m@mtmail.mtsu.edu>
2016-11-10 19:36:31 -0600

    display graph

--- LogiCane/app/src/main/java/com/versatiledevs/logicane/Data_visualization.java
+++ LogiCane/app/src/main/java/com/versatiledevs/logicane/Data_visualization.java
@@ -1,5 +1,6 @@
 package com.versatiledevs.logicane;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -51,6 +52,8 @@ public class Data_visualization extends AppCompatActivity {
     // Array for the time
     private ArrayList<String> array_StringTS = new ArrayList<>();
     private ArrayList<Double> array_DoubleTS = new ArrayList<>();
+    private ArrayList<Double> array_Milliseconds = new ArrayList<>();
+
 
     // Array for the force signal
     private ArrayList<Double> array_f0 = new ArrayList<>();
@@ -151,7 +154,6 @@ public class Data_visualization extends AppCompatActivity {
         maxHandleAbsolute   = (TextView) findViewById(R.id.maxHandleAbsolute);
         maxBaseTrans        = (TextView) findViewById(R.id.maxBaseTransverse);
         maxBaseAbsolute     = (TextView) findViewById(R.id.maxBaseAbsolute);
-
 
         //GraphView graph = (GraphView) findViewById(R.id.displayView);
 
@@ -332,9 +334,6 @@ public class Data_visualization extends AppCompatActivity {
 
         /***  FILLING THE TABLE: HANDLE ****/
 
-        /***  Traverse Plane Acceleration Magnitude ( Traverse Acceleration "blue")  ***/
-        /***  Acceleration Magnitude  ( Absolute Acceleration "Green" ) ***/
-
         //function calls
         convertTimeMilliseconds();
         accelerationMagnitude();
@@ -346,59 +345,92 @@ public class Data_visualization extends AppCompatActivity {
 
         // mean
         meanHandleTrans.setText(decimalFormat.format(mean(array_HA_TransvPlaneAccelMagn)));
-
         meanHandleAbsolute.setText(decimalFormat.format(mean(array_HA_AccelMagnitude)));
-
         meanBaseTrans.setText(decimalFormat.format(mean(array_BA_TransvPlaneAccelMagn)));
-
         meanBaseAbsolute.setText(decimalFormat.format(mean(array_BA_AccelMagnitude)));
 
         //SD
         sdHandleTrans.setText(decimalFormat.format(standardDeviation(array_HA_TransvPlaneAccelMagn)));
-
         sdHandleAbsolute.setText(decimalFormat.format(standardDeviation(array_HA_AccelMagnitude)));
-
         sdBaseTrans.setText(decimalFormat.format(standardDeviation(array_BA_TransvPlaneAccelMagn)));
-
         sdBaseAbsolute.setText(decimalFormat.format(standardDeviation(array_BA_AccelMagnitude)));
 
 
         // median
         medianHandleTrans.setText(decimalFormat.format(median(array_HA_TransvPlaneAccelMagn)));
-
         medianHandleAbsolute.setText(decimalFormat.format(median(array_HA_AccelMagnitude)));
-
         medianBaseTrans.setText(decimalFormat.format(median(array_BA_TransvPlaneAccelMagn)));
-
         medianBaseAbsolute.setText(decimalFormat.format(median(array_BA_AccelMagnitude)));
 
 
         // min
         minHandleTrans.setText(decimalFormat.format(min(array_HA_TransvPlaneAccelMagn)));
-
         minHandleAbsolute.setText(decimalFormat.format(min(array_HA_AccelMagnitude)));
-
         minBaseTrans.setText(decimalFormat.format(min(array_BA_TransvPlaneAccelMagn)));
-
         minBaseAbsolute.setText(decimalFormat.format(min(array_BA_AccelMagnitude)));
 
         // max
         maxHandleTrans.setText(decimalFormat.format(max(array_HA_TransvPlaneAccelMagn)));
-
         maxHandleAbsolute.setText(decimalFormat.format(max(array_HA_AccelMagnitude)));
-
         maxBaseTrans.setText(decimalFormat.format(max(array_BA_TransvPlaneAccelMagn)));
-
         maxBaseAbsolute.setText(decimalFormat.format(max(array_BA_AccelMagnitude)));
 
 
-
-        //meanHandleTrans.setText(decimalFormat.format( mean(array_HA_TransvPlaneAccelMagn) ));
-        //meanHandleAbsolulte.setText(decimalFormat.format(mean(array_BA_TransvPlaneAccelMagn)));
+        GraphView graph = (GraphView) findViewById(R.id.displayView);
 
 
+        LineGraphSeries <DataPoint> series1 = new LineGraphSeries<>();
+        for(int i =0; i<array_Milliseconds.size(); i++) {
+
+            series1.appendData(new DataPoint(i, array_HA_TransvPlaneAccelMagn.get(i)), true, 100);
+        }
 
 
+        LineGraphSeries <DataPoint> series2 = new LineGraphSeries<>();
+        for(int i =0; i<array_Milliseconds.size(); i++) {
+
+            series2.appendData(new DataPoint(i, array_HA_AccelMagnitude.get(i)), true, 100);
+        }
+
+
+        LineGraphSeries <DataPoint> series3 = new LineGraphSeries<>();
+        for(int i =0; i<array_Milliseconds.size(); i++) {
+
+            series3.appendData(new DataPoint(i, array_BA_TransvPlaneAccelMagn.get(i)), true, 100);
+        }
+
+
+        LineGraphSeries <DataPoint> series4 = new LineGraphSeries<>();
+        for(int i =0; i<array_Milliseconds.size(); i++) {
+
+            series4.appendData(new DataPoint(i, array_BA_AccelMagnitude.get(i)), true, 100);
+        }
+
+
+        // set manual X bounds
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(3);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(array_Milliseconds.size()+20);
+
+
+        // This option does not work on my laptop
+        graph.getViewport().setScrollable(true);    // enables horizontal scrolling
+        graph.getViewport().setScrollableY(true);   // enables vertical scrolling
+        graph.getViewport().setScalable(true);      // enables horizontal zooming and scrolling
+        graph.getViewport().setScalableY(true);     // enables vertical zooming and scrolling
+
+        series2.setColor(Color.RED);
+        series3.setColor(Color.BLUE);
+        series4.setColor(Color.GREEN);
+
+        graph.addSeries(series1);
+        graph.addSeries(series2);
+        graph.addSeries(series3);
+        graph.addSeries(series4);
 
     } //end of data
 
@@ -422,7 +454,7 @@ public class Data_visualization extends AppCompatActivity {
 
         // convert the time in milliseconds
         for (int i = 0; i < array_DoubleTS.size(); i+=4)
-            Dtest.add((array_DoubleTS.get(i)*60*60*60 )+(array_DoubleTS.get(i+1) * 60*60) +(array_DoubleTS.get(i+2) *60 ) + array_DoubleTS.get(i+3));
+            array_Milliseconds.add((array_DoubleTS.get(i)*60*60*60 )+(array_DoubleTS.get(i+1) * 60*60) +(array_DoubleTS.get(i+2) *60 ) + array_DoubleTS.get(i+3));
 
     }
 
